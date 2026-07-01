3a419105620ebc34787032ad23fb9472943bf62e
kirusyag <xgalat@stuba.sk>
2026-07-01 19:48:48 +0200

    add function v1

--- project.c
+++ project.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE 600;
-#define MAX_TOKEN 100;
+#define MAX_LINE 600
+#define MAX_TOKEN 100
 
 char* getField(const char *line,  int index){
     int curr;
@@ -73,5 +73,68 @@ int openFiles(FILE **fS, FILE **fH, FILE **fR){
     *fH = h;
     *fR = r;    
     return 1;
+}
+
+void v1(FILE *fH, FILE *fR) {
+    char lineH[MAX_LINE];
+    char lineR[MAX_LINE];
+    int first;
+    char *pid;
+    char *name;
+    char *krajina;
+    char *rok;
+    char copy[MAX_LINE];
+    char *pidR;
+    char *pTrim;
+    int count;
+
+    first = 1;
+
+    rewind(fH);
+
+    while (fgets(lineH, sizeof(lineH), fH)) {
+        trim(lineH);
+        if (strlen(lineH) == 0) {
+            continue;
+        }
+
+        pid = getField(lineH, 0);
+        name = getField(lineH, 1);
+        krajina = getField(lineH, 2);
+        rok = getField(lineH, 3);
+
+        if (!first) printf("\n");
+
+        first = 0;
+
+        printf("Identifikator: %s\n", pid ? pid : " ");
+        printf("Meno a priezvisko: %s\n", name ? name : " ");
+        printf("Krajina: %s\n", krajina ? krajina : " ");
+        printf("Rok: %s\n", rok ? rok : " ");
+        printf("Riesenia: \n");
+
+        rewind(fR);
+        count = 0;
+        while( count < 10 && fgets(lineR, sizeof(lineR), fR)) {
+            srtncpy(copy, lineR, MAX_LINE - 1);
+            copy[MAX_LINE - 1] = '\0';
+            trim(copy);
+            if (strlen(copy) == 0) continue;
+
+            pidR = getField(copy, 0);
+            if (pidR) {
+                pTrim = skipSpaces(pidR);
+                if (pid && strcmp(pTrim, pid) == 0) {
+                    printf("\t%s\n", copy);
+                    count++;
+                }
+                free(pidR);
+            }
+        }
+        if (pid) free(pid);
+        if (name) free(name);
+        if (krajina) free(krajina);
+        if (rok) free(rok);
+    }
 }
 
